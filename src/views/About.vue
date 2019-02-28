54adc6e542522f8e035d954b2af517527f782a02
zhangjingfeng <734546749@qq.com>
2019-02-28 15:00:13 +0800

    初始化

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is a about page</h1>
+    <el-date-picker
+      v-model="value1"
+      type="datetime"
+      placeholder="选择日期时间">
+    </el-date-picker>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      value1: ''
+    }
+  }
+}
+</script>
